9013a1aa9f43319309af9941b80dca850b30cb3a
Kenny <krluna@gmail.com>
2015-03-17 21:17:29 -0400

    complete mapper

--- hw3/Task1/map.py
+++ hw3/Task1/map.py
@@ -13,20 +13,24 @@ for line in sys.stdin:
 		hack_license = splits[1]
 		vendor_id = splits[2]
 		pickup_datetime = splits[3]
-
-		id = (medallion, hack_license, vendor_id, pickup_datetime) 
+		id = (medallion, hack_license, vendor_id, pickup_datetime)
+		 
 		vals = ",".join(splits[4:]) 
 		print "(%s,%s,%s,%s)\t(%s)" % (id[0], id[1], id[2], id[3], vals)  		
 	
 	else:
 
 		medallion = splits[0]
-                hack_license = spits[1]
+                hack_license = splits[1]
                 vendor_id = splits[2]
-		rate_code, store_and_fwd_flag = splits[3:5]
 		pickup_datetime = splits[5]
+		
+		rate_code, store_and_fwd_flag = splits[3:5]
+
 		dropoff_datetime, passenger_count, trip_time_in_secs, trip_distance, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude = splits[6:]
 
 		id = (medallion, hack_license, vendor_id, pickup_datetime)
-
-		 
+		
+		print "(%s,%s,%s,%s)\t(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)" % (id[0], id[1], id[2], id[3], rate_code, store_and_fwd_flag, \
+							dropoff_datetime, passenger_count, trip_time_in_secs, trip_distance, pickup_longitude, \
+							pickup_latitude, dropoff_longitude, dropoff_latitude)
