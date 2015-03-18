0d2f793bb8b6f1cd498114f4f27aae4a5eea78a8
Kenny <krluna@gmail.com>
2015-03-17 20:57:23 -0400

    task1 for hw3

---
+++ hw3/Task1/map.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+
+for line in sys.stdin:
+
+	line = line.strip() 
+	splits = line.split(",")
+
+	if len(splits) == 11: 
+		
+		medallion = splits[0]
+		hack_license = splits[1]
+		vendor_id = splits[2]
+		pickup_datetime = splits[3]
+
+		id = (medallion, hack_license, vendor_id, pickup_datetime) 
+		vals = ",".join(splits[4:]) 
+		print "(%s,%s,%s,%s)\t(%s)" % (id[0], id[1], id[2], id[3], vals)  		
+	
+	else:
+
+		medallion = splits[0]
+                hack_license = spits[1]
+                vendor_id = splits[2]
+		rate_code, store_and_fwd_flag = splits[3:5]
+		pickup_datetime = splits[5]
+		dropoff_datetime, passenger_count, trip_time_in_secs, trip_distance, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude = splits[6:]
+
+		id = (medallion, hack_license, vendor_id, pickup_datetime)
+
+		 
