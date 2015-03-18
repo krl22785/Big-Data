087d9c605ec24b78f8698f4d75e2695d347dccd2
Kenny <krluna@gmail.com>
2015-03-18 18:56:52 -0400

    modifications to reducer

--- hw3/Task1/reduce.py
+++ hw3/Task1/reduce.py
@@ -4,15 +4,44 @@ import sys
 
 
 current_key = None 
+current_fares = [0]
+current_trips = [0] 
+finalAttributes = [] 
 
 for line in sys.stdin:
 	
-	print type(line) 
-	#k, v = line.split("\t")
+	key, value = line.split('\t')				 
+	value = eval(value)
 
-	#if key == current_key 	 
+	tableName = value[0]
+
+	tempAttributes = value[1]
+	tableAttributes = tempAttributes.split(",") 
 	
+	if key == current_key:
+
+		if tableName == 'fares':
+                        current_fares = tableAttributes
+                else:
+                        current_trips = tableAttributes
+	else:
+		
+		if current_key:  
+			#print "%s\t%s" % (current_key, finalAttributes) 
+			allAttributes =  [] 
+			allAttributes.extend(current_fares)
+			allAttributes.extend(current_trips)  
+			
+			print "%s\t%s" % (key, allAttributes) 
 	
-
-
-
+		current_key = key 
+		if tableName == 'fares':
+                        current_fares = tableAttributes
+                else:
+                        current_trips = tableAttributes
+		
+		
+if current_key == key:
+	allAttributes =  []
+        allAttributes.extend(current_fares)
+        allAttributes.extend(current_trips)
