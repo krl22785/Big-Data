46a14de6a50a8a538f58b8dfb08f8a13e5f2eb12
Kenny <krluna@gmail.com>
2015-03-17 22:38:01 -0400

    updated mapper, and reducer

--- hw3/Task1/map.py
+++ hw3/Task1/map.py
@@ -1,36 +1,38 @@
 #!/usr/bin/python
 
 import sys
+import os
+
 
 for line in sys.stdin:
-
+	
+	#env = os.chdir['HOME']
+	#env = os.environ["mapreduce_map_input_file"]	
+ 	
 	line = line.strip() 
 	splits = line.split(",")
 
 	if len(splits) == 11: 
 		
-		medallion = splits[0]
-		hack_license = splits[1]
-		vendor_id = splits[2]
-		pickup_datetime = splits[3]
-		id = (medallion, hack_license, vendor_id, pickup_datetime)
-		 
-		vals = ",".join(splits[4:]) 
-		print "(%s,%s,%s,%s)\t(%s)" % (id[0], id[1], id[2], id[3], vals)  		
-	
+		keyAttributes = splits[0:4]
+		otherAttributes = splits[4:] 
+		valuePair = ("fares", otherAttributes) 
+		
+		print "%s\t%s" % (keyAttributes, valuePair) 
+		
 	else:
-
-		medallion = splits[0]
-                hack_license = splits[1]
-                vendor_id = splits[2]
-		pickup_datetime = splits[5]
 		
+		keyAttributes = [] 
+		keyAttributes.append(splits[0])
+                keyAttributes.append(splits[1])
+                keyAttributes.append(splits[2])
+		keyAttributes.append(splits[5])
+			
+		otherAttributes = []
 		rate_code, store_and_fwd_flag = splits[3:5]
-
-		dropoff_datetime, passenger_count, trip_time_in_secs, trip_distance, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude = splits[6:]
-
-		id = (medallion, hack_license, vendor_id, pickup_datetime)
-		
-		print "(%s,%s,%s,%s)\t(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)" % (id[0], id[1], id[2], id[3], rate_code, store_and_fwd_flag, \
-							dropoff_datetime, passenger_count, trip_time_in_secs, trip_distance, pickup_longitude, \
-							pickup_latitude, dropoff_longitude, dropoff_latitude)
+		otherAttributes.append(rate_code)
+		otherAttributes.append(store_and_fwd_flag)
+		otherAttributes.extend(splits[6:])
+		valuePair = ("trips", otherAttributes) 	
+	
+		print "%s\t%s" % (keyAttributes, valuePair)
---
+++ hw3/Task1/reduce.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+
+
+current_key = None 
+
+for line in sys.stdin:
+	
+	print type(line) 
+	#k, v = line.split("\t")
+
+	#if key == current_key 	 
+	
+	
+
+
+
