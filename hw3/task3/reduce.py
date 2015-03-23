a725e9e8525d8f071b0483e46a234d11bf4146b7
Kenny <krluna@gmail.com>
2015-03-23 08:00:37 -0400

    update

--- hw3/task3/reduce.py
+++ hw3/task3/reduce.py
@@ -4,12 +4,12 @@ import sys
 
 current_key = None
 current_fares = []
-current_trips = []
+current_trips = [] 
+storage = {} 
 n = 0 
-final_output = {} 
 
 for line in sys.stdin:
-	
+
         key, value = line.split('\t')
         value = eval(value)
 
@@ -18,45 +18,57 @@ for line in sys.stdin:
         tempAttributes = value[1]
         tableAttributes = tempAttributes.split(",")
 
-	if tableName != "licenses":
-
-		if key == current_key:
+	if tableName != 'licenses':  
+        	if key == current_key:
 	
                 	if tableName == 'fares':
                         	current_fares = tableAttributes
-                	else:
+               		else:
                         	current_trips = tableAttributes
+        	else:  
+ 			 
+               		allAttributes = []
+                	allAttributes.extend(current_trips)
+                	allAttributes.extend(current_fares)
+		
+			if len(allAttributes) == 17:  #make sure each key appears in both 
 
-			allAttributes = []
-                        allAttributes.extend(current_trips)
-                        allAttributes.extend(current_fares)
-
-			if len(allAttributes) == 17:
-                                final_output[current_key] = allAttributes
-                                current_trips = []
-                                current_fares = []
-			else:
+                	#print "%s\t%s" % (current_key, allAttributes)
+			
+				storage[current_key] = allAttributes 
 				current_trips = [] 
-				current_fares = [] 
+				current_fares = [] 	
+				current_key = key
+	
+				if tableName == 'fares':
+        		               	current_fares = tableAttributes
+               			else:
+                       			current_trips = tableAttributes
+			else: 
+				current_trips = []
+                        	current_fares = []
+                        	current_key = key
 
-        	else: 
-			current_key = key 
+                        	if tableName == 'fares':
+                                	current_fares = tableAttributes
+                        	else:
+                                	current_trips = tableAttributes
+	else:
 
-			if tableName == 'fares':
-                               	current_fares = tableAttributes
-                      	else:
-                               	current_trips = tableAttributes 
-		 
-	else:	
-		for item in final_output.items():
-#			n += 1
-#			print str(n), key, item[0]
-
-		 	s1 = [i for i in eval(item[0])]
+		for item in storage.items():
+			s1 = [i for i in eval(item[0])] 
 			s1.extend(item[1]) 
 			s1.extend(tableAttributes) 
 			n += 1
-			print "%s\t%s" % (key, s1[1:])
- 
-		final_output = {} 
-print n 		 
+			print "%s\t%s" % (s1[0], s1[1:])  
+
+		storage = {} 
+			
+print n 
+
+if current_key == key: 
+	allAttributes = []
+        allAttributes.extend(current_trips)
+        allAttributes.extend(current_fares)
+        print "%s\t%s" % (current_key, allAttributes)
+
